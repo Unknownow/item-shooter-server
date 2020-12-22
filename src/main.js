e8c553ad3faba3654a6ed6e039d589285ca1c39a
Cường Bùi <cuong.bpt82@gmail.com>
2020-12-22 23:22:26 +0700

    sửa để deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 2882
+const PORT = process.env.PORT || 5000
 var cors = require('cors')
 
 app.use(cors())
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
